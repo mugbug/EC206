44d37144a052f0708499d3c26859b90e3585aae1
Pedro Zaroni <zaronipedro@outlook.com>
2017-03-18 17:58:44 -0300

    Add blank field checking and field cleaner

--- GUI/gui_listener.py
+++ GUI/gui_listener.py
@@ -4,5 +4,16 @@ from app.crud import ClientIO
 class ButtonListener(object):
 
     @staticmethod
-    def add_client(name, address, cpf, age):
-        ClientIO.add(str(name.text()), str(address.text()), str(cpf.text()), int(age.text()))
+    def add_client(name_field, address_field, cpf_field, age_field):
+        name = str(name_field.text())
+        address = str(address_field.text())
+        cpf = str(cpf_field.text())
+        age = int(age_field.text())
+        if (name and address and cpf and age) != '':
+            ClientIO.add(name, address, cpf, age)
+            name_field.setText('')
+            address_field.setText('')
+            cpf_field.setText('')
+            age_field.setValue(0)
+        else:
+            print 'ERROR: All fields must be filled!'
