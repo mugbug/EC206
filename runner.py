0d2ea53634e2421179198779b833289862d1eec9
Pedro Zaroni <zaronipedro@outlook.com>
2017-03-30 22:32:35 -0300

    Add connection with GUI file

--- runner.py
+++ runner.py
@@ -1,13 +1,22 @@
 # coding: UTF-8
 import sys
-from view.main_menu import UIMainWindow
 from PyQt4 import QtGui
+
+import view.design as ui
+from controller.gui_listener import ButtonListener
+
+
+class Application(QtGui.QMainWindow, ui.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(Application, self).__init__(parent)
+        self.setupUi(self)
+        self.widget_add.setVisible(False)
+        ButtonListener.action_listener(self)
+
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     app.setStyle(QtGui.QStyleFactory.create('plastique'))
-    main_window = QtGui.QMainWindow()
-    ui = UIMainWindow()
-    ui.setupUi(main_window)
-    main_window.show()
-    sys.exit(app.exec_())
+    form = Application()
+    form.show()
+    app.exec_()
