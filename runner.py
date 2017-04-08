8c47e870559a4020127e4b352c7f6958b037f6f3
Pedro Zaroni <zaronipedro@outlook.com>
2017-04-07 22:42:05 -0300

    Add some initial configurations

--- runner.py
+++ runner.py
@@ -3,15 +3,41 @@ import sys
 from PyQt4 import QtGui
 
 import view.design as ui
-from controller.gui_listener import ButtonListener
+from controller.gui_listener import ButtonListener, SwitchWidget
 
 
 class Application(QtGui.QMainWindow, ui.Ui_MainWindow):
     def __init__(self, parent=None):
         super(Application, self).__init__(parent)
         self.setupUi(self)
-        self.widget_add.setVisible(False)
+        self.config_ui()
+
+    def config_ui(self):
+        self.configure_tables()
+        SwitchWidget.to_login(self)
         ButtonListener.action_listener(self)
+
+    def configure_tables(self):
+        # home table
+        header = self.home_table.horizontalHeader()
+        header.setResizeMode(0, QtGui.QHeaderView.Stretch)
+        header.setResizeMode(1, QtGui.QHeaderView.ResizeToContents)
+        header.setResizeMode(2, QtGui.QHeaderView.ResizeToContents)
+        header.setResizeMode(3, QtGui.QHeaderView.ResizeToContents)
+        header.setResizeMode(4, QtGui.QHeaderView.ResizeToContents)
+        header.setResizeMode(5, QtGui.QHeaderView.ResizeToContents)
+        # client table
+        header = self.client_table.horizontalHeader()
+        header.setResizeMode(0, QtGui.QHeaderView.ResizeToContents)
+        header.setResizeMode(1, QtGui.QHeaderView.ResizeToContents)
+        header.setResizeMode(2, QtGui.QHeaderView.Stretch)
+        header.setResizeMode(3, QtGui.QHeaderView.ResizeToContents)
+        # equipment table
+        header = self.equipment_table.horizontalHeader()
+        header.setResizeMode(0, QtGui.QHeaderView.Stretch)
+        header.setResizeMode(1, QtGui.QHeaderView.ResizeToContents)
+        header.setResizeMode(2, QtGui.QHeaderView.ResizeToContents)
+        header.setResizeMode(3, QtGui.QHeaderView.ResizeToContents)
 
 
 if __name__ == '__main__':
