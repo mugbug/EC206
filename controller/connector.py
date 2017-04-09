b2473ce10cf7423866721c025a23f98b9471bfeb
Pedro Zaroni <zaronipedro@outlook.com>
2017-04-08 22:06:44 -0300

    Changed database connection config.

--- controller/connector.py
+++ controller/connector.py
@@ -1,10 +1,25 @@
 import MySQLdb
+from PyQt4.QtGui import QMessageBox
 
 
 class DataBaseConnector(object):
 
     @staticmethod
-    def get_db():
-        db = MySQLdb.connect("localhost", "root", "root", "CEMIG")
-        return db
+    def get_db(app):
+        db_connection = False
+        try:
+            db = MySQLdb.connect(host='localhost', user='root', passwd='', db='mydb')
+        except MySQLdb.Error as err:
+            e = "Database connection error [%d]: %s" % (err.args[0], err.args[1])
+        else:
+            db_connection = True
+        finally:
+            if db_connection:
+                print 'Database connection successful!'
+                QMessageBox.information(app, 'Notification', 'Database connection successful!')
+                return db
+            else:
+                print e
+                QMessageBox.critical(app, 'Error!', 'Error trying to connect to database!\n{0}'.format(e))
+                return 1
 
