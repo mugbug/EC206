7ab406506391237bbc6af68d20601fa90e923bd3
Pedro Zaroni <zaronipedro@outlook.com>
2017-04-08 22:08:37 -0300

    Add Client CRUD listener methods

--- controller/gui_listener.py
+++ controller/gui_listener.py
@@ -1,3 +1,5 @@
+from PyQt4 import QtGui
+
 from models.crud import *
 
 
@@ -13,107 +15,86 @@ class ButtonFeatures(object):
         SwitchWidget.to_login(app)
         # log out
 
+    # _________________HOME_______________________
     @staticmethod
-    def add_client(app):
-        class_name = str(app.lbl_path_class.text())
-        if class_name == 'Client':
-            name = str(app.form_input_1.text())
-            age = int(app.form_input_2.text())
-            address = str(app.form_input_3.text())
-            cpf = str(app.form_input_4.text())
-            if (name and address and cpf and age) != '':
-                ClientIO.add(name, address, cpf, age)
-                # field cleaner
-                app.form_input_1.setText('')
-                app.form_input_2.setText('')
-                app.form_input_3.setText('')
-                app.form_input_4.setText('')
-            else:
-                print 'ERROR: All fields must be filled!'
+    def export_pdf(app):
+        pass
+    #     file_name = QtCore.QString('report.pdf')
+    #
+    #     printer = QtGui.QPrinter(QtGui.QPrinter.HighResolution)
+    #     printer.setOutputFileName(file_name)
+    #     printer.setPaperSize(QtGui.QPrinter.A4)
+    #     printer.setOutputFormat(QtGui.QPrinter.PdfFormat)
+    #
+    #     painter = QtGui.QPainter(app)
+    #     painter.setRenderHint(QtGui.QPainter.Antialiasing | QtGui.QPainter.TextAntialiasing
+    #                           | QtGui.QPainter.SmoothPixmapTransform)
+    #     app.home_table.render(painter)
+    #     painter.end()
 
-        if class_name == 'Manager':
-            name = str(app.form_input_1.text())
-            email = str(app.form_input_2.text())
-            if (name and email) != '':
-                ManagerIO.add(name, email)
+    # _________________CLIENT MANAGER_____________
+    @staticmethod
+    def client_create(app):
+        name = str(app.client_input_name.text())
+        age = app.client_input_age.value()
+        address = str(app.client_input_address.text())
+        cpf = str(app.client_input_cpf.text())
+        if (name and address and cpf and age) != '':
+            result = ClientIO.add(name, address, cpf, age, app)
+            if result == 0:
                 # field cleaner
-                app.form_input_1.setText('')
-                app.form_input_2.setText('')
-                app.form_input_3.setText('')
-                app.form_input_4.setText('')
-            else:
-                print 'ERROR: All fields must be filled!'
-
-        if class_name == 'Equipment':
-            model = str(app.form_input_1.text())
-            brand = str(app.form_input_2.text())
-            consumption = str(app.form_input_3.text())
-            if (model and brand and consumption) != '':
-                EquipmentIO.add(model, brand, consumption)
-                # field cleaner
-                app.form_input_1.setText('')
-                app.form_input_2.setText('')
-                app.form_input_3.setText('')
-                app.form_input_4.setText('')
-            else:
-                print 'ERROR: All fields must be filled!'
-
-        if class_name == 'Agency':
-            city = str(app.form_input_1.text())
-            address = str(app.form_input_2.text())
-            manager = str(app.form_input_3.text())
-            if (city and address and manager) != '':
-                AgencyIO.add(city, address, manager)
-                # field cleaner
-                app.form_input_1.setText('')
-                app.form_input_2.setText('')
-                app.form_input_3.setText('')
-                app.form_input_4.setText('')
-            else:
-                print 'ERROR: All fields must be filled!'
-
-        if class_name == 'Consumption':
-            time = str(app.form_input_1.text())
-            consumption = str(app.form_input_2.text())
-            if (time and consumption) != '':
-                ConsumptionIO.add(time, consumption)
-                # field cleaner
-                app.form_input_1.setText('')
-                app.form_input_2.setText('')
-                app.form_input_3.setText('')
-                app.form_input_4.setText('')
-            else:
-                print 'ERROR: All fields must be filled!'
-
-        if class_name == 'Support':
-            call = str(app.form_input_1.text())
-            protocol = str(app.form_input_2.text())
-            if (call and protocol) != '':
-                SupportIO.add(call, protocol)
-                # field cleaner
-                app.form_input_1.setText('')
-                app.form_input_2.setText('')
-                app.form_input_3.setText('')
-                app.form_input_4.setText('')
-            else:
-                print 'ERROR: All fields must be filled!'
+                app.client_input_name.clear()
+                app.client_input_age.clear()
+                app.client_input_address.clear()
+                app.client_input_cpf.clear()
+                # add item to table
+                row_position = app.client_table.rowCount()
+                app.client_table.insertRow(row_position)
+                app.client_table.setItem(row_position, 0, QtGui.QTableWidgetItem(name))
+                app.client_table.setItem(row_position, 1, QtGui.QTableWidgetItem(str(age)))
+                app.client_table.setItem(row_position, 2, QtGui.QTableWidgetItem(address))
+                app.client_table.setItem(row_position, 3, QtGui.QTableWidgetItem(cpf))
+        else:
+            print 'ERROR: All fields must be filled!'
 
     @staticmethod
-    def cancel(app):
-        app.widget_add.setVisible(False)
-        app.widget_login.setVisible(True)
+    def client_edit(app):
+        selected_item = app.client_table.currentRow()
+        name = app.client_table.item(selected_item, 0).text()
+        age = int(app.client_table.item(selected_item, 1).text())
+        address = app.client_table.item(selected_item, 2).text()
+        cpf = app.client_table.item(selected_item, 3).text()
+        app.client_input_name.setText(name)
+        app.client_input_age.setValue(age)
+        app.client_input_address.setText(address)
+        app.client_input_cpf.setText(cpf)
+
+    @staticmethod
+    def client_delete(app):
+        selected_item = app.client_table.currentRow()
+        cpf = app.client_table.item(selected_item, 3).text()
+        ClientIO.delete(cpf)
+        app.client_table.removeRow(selected_item)
 
 
 class ButtonListener(object):
     @staticmethod
     def action_listener(app):
-        # _________________MENU BAR___________________
+        # _________________MENU BAR______________________
         app.menu_file_logout.triggered.connect(lambda: ButtonFeatures.log_out(app))
         app.menu_settings_manage.triggered.connect(lambda: SwitchWidget.to_crud(app))
 
         # _________________LOGIN BUTTONS_________________
         app.login_btn_login.clicked.connect(lambda: ButtonFeatures.log_in(app))
         app.btn_home.clicked.connect(lambda: SwitchWidget.to_home(app))
+
+        # _________________HOME__________________________
+        app.home_btn_generate_report.clicked.connect(lambda: ButtonFeatures.export_pdf(app))
+
+        # _________________CLIENT MANAGING_______________
+        app.client_btn_create.clicked.connect(lambda: ButtonFeatures.client_create(app))
+        app.client_btn_edit.clicked.connect(lambda: ButtonFeatures.client_edit(app))
+        app.client_btn_delete.clicked.connect(lambda: ButtonFeatures.client_delete(app))
 
 
 class SwitchWidget(object):
