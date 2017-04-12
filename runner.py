7d2eaf1e68bc427f97bbbb324703080cef667ee3
Pedro Zaroni <zaronipedro@outlook.com>
2017-04-12 00:03:42 -0300

    Add sidebar initial configurations

--- runner.py
+++ runner.py
@@ -1,9 +1,8 @@
 # coding: UTF-8
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 import view.design as ui
-from controller.gui_listener import ButtonListener, SwitchWidget
 from controller.connector import DataBaseConnector
 
 
@@ -18,14 +17,26 @@ class Application(QtGui.QMainWindow, ui.Ui_MainWindow):
         return self.db
 
     def config_ui(self):
+        # Some last GUI configs
         self.configure_tables()
+        from controller.gui_listener import ButtonListener, SwitchWidget
         SwitchWidget.to_login(self)
         ButtonListener.action_listener(self)
+        # Expands sidebar items
+        self.sidebar.topLevelItem(0).setExpanded(True)
+        self.sidebar.topLevelItem(1).setExpanded(True)
+        # Validators
+        regex_cpf = QtCore.QRegExp('\d{1,3}\.\d{1,3}\.\d{1,3}\-\d{1,2}$')
+        validator_cpf = QtGui.QRegExpValidator(regex_cpf)
+        self.login_input_cpf.setValidator(validator_cpf)
+        # or
+        # InputMask
+        # self.login_input_cpf.setInputMask('999.999.999-99;')
 
     def configure_tables(self):
         # home table
         header = self.home_table.horizontalHeader()
-        header.setResizeMode(0, QtGui.QHeaderView.Stretch)
+        header.setResizeMode(0, QtGui.QHeaderView.ResizeToContents)
         header.setResizeMode(1, QtGui.QHeaderView.ResizeToContents)
         header.setResizeMode(2, QtGui.QHeaderView.ResizeToContents)
         header.setResizeMode(3, QtGui.QHeaderView.ResizeToContents)
@@ -48,6 +59,8 @@ class Application(QtGui.QMainWindow, ui.Ui_MainWindow):
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     app.setStyle(QtGui.QStyleFactory.create('plastique'))
+    # import style
+    # app.setStyleSheet(style.style)
     form = Application()
     form.show()
     app.exec_()
