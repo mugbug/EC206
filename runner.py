a11b3e57529b4055f52760a649188e25f7732771
Pedro Zaroni <zaronipedro@outlook.com>
2017-04-23 19:40:29 -0300

    Add window move action

--- runner.py
+++ runner.py
@@ -16,18 +16,36 @@ class Application(QtGui.QMainWindow, ui.Ui_MainWindow):
         UiConfig.config_ui(self)
         self.db = DataBaseConnector.get_db(self)
 
+        self.leftClick = False
+        # self.menubar.mouseMoveEvent = self.mouseMoveEvent
+        # self.menubar.mousePressEvent = self.mousePressEvent
+        # self.menubar.mouseReleaseEvent = self.mouseReleaseEvent
+
     def closeEvent(self, event):
         self.db.close()
 
     def get_db(self):
         return self.db
 
+    def mouseMoveEvent(self, event):
+        if self.leftClick == True:
+            x=event.globalX()
+            y=event.globalY()
+            x_w = self.offset.x()
+            y_w = self.offset.y()
+            self.move(x-x_w, y-y_w)
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self.leftClick = True
+            self.offset = event.pos()
+
+    def mouseReleaseEvent(self, event):
+        self.leftClick = False
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     app.setStyle(QtGui.QStyleFactory.create('plastique'))
-    # import style
-    # app.setStyleSheet(style.style)
     form = Application()
     form.setWindowFlags(QtCore.Qt.FramelessWindowHint)
     form.show()
